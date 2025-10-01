c87acfbc8684278e021a48508521e3548f882e15
Nnder <asfahaikrkt6k56@yandex.ru>
2025-10-01 22:39:37 +0300

    тестовая таблица

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,39 @@
+<script lang="ts" setup></script>
+
 <template>
-  <HelloWorld />
+  <div>
+    <div>
+      <span>Учетные записи</span>
+      <v-icon-btn variant="text" icon="mdi-close"></v-icon-btn>
+    </div>
+
+    <div>
+      <v-container class="bg-surface-variant mb-6">
+        <v-row align="start" no-gutters v-for="n in 3" :key="n">
+          <v-col>
+            <v-text-field label="Метки"></v-text-field>
+          </v-col>
+          <v-col>
+            <v-select
+              label="Тип записи"
+              :items="['Локальная', 'LDAP']"
+            ></v-select>
+          </v-col>
+          <v-col>
+            <v-text-field label="Логин"></v-text-field>
+          </v-col>
+          <v-col>
+            <v-text-field label="Пароль"></v-text-field>
+          </v-col>
+          <v-col>
+            <v-icon-btn variant="text" icon="mdi-trash-can"></v-icon-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-  //
+//
 </script>
