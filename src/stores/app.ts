6af8c1b62682544a05a938594b39337acb56d1ca
Nnder <asfahaikrkt6k56@yandex.ru>
2025-10-02 01:17:17 +0300

    сохраненеие в store

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -14,5 +14,18 @@ export const useUsersStore = defineStore("users", () => {
     users.value = filteredUsers;
   };
 
-  return { users, userCounter, addUser, removeUser };
+  const save = () => {
+    const validData = users.value.filter((user) => user.save === true);
+    localStorage.setItem("users", JSON.stringify(validData));
+    console.log(validData, "for save");
+  };
+
+  const restoreLocalStorage = () => {
+    const data = JSON.parse(localStorage.getItem("users"));
+    users.value = data;
+  };
+
+  restoreLocalStorage();
+
+  return { users, userCounter, addUser, removeUser, save, restoreLocalStorage };
 });
